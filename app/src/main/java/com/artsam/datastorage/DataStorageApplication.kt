cc83e727890a4f2ec7e5ec267a89822c725057a3
artsam <samoshkin.job@gmail.com>
2022-06-23 14:07:55 +0300

    - Module Presentation created. Gradle files optimised.

---
+++ app/src/main/java/com/artsam/datastorage/DataStorageApplication.kt
@@ -0,0 +1,10 @@
+package com.artsam.datastorage
+
+import android.app.Application
+
+class DataStorageApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
--- presentation/src/main/java/com/artsam/presentation/ui/activity/MainActivity.kt
+++ presentation/src/main/java/com/artsam/presentation/ui/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.artsam.datastorage
+package com.artsam.presentation.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.artsam.presentation.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
